50a3977ce3919287a71616beb0e5b28b240573bc
Jose Velasquez <velas.joseignacio@gmail.com>
2025-10-12 22:40:14 -0300

    Se arregla footer, se agregan más datos a este. Se cambia color de letras a productos y se agrega la funcionalidad de clickear e ir a cada producto en específico.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,12 +3,31 @@ import "../styles/navbar_style.css";
 export default function Footer() {
   return (
     <footer>
-      <h3>Contacto</h3>
-      <ul>
-        <li>Teléfono: +569 123 456 89</li>
-        <li>Correo: tcg@contacto.cl</li>
-        <li>Dirección: Álvarez 1136</li>
-      </ul>
+      <div className="footer-container">
+        <div className="footer-links">
+          <ul>
+            <li>Inicio</li>
+            <li>Preguntas frecuentes</li>
+            <li>Ubicación</li>
+            <li>Contacto</li>
+          </ul>
+        </div>
+        <div className="footer-logo">
+          {/* Reemplaza la ruta por la de tu logo */}
+          <img
+            src="https://static.wixstatic.com/media/cc32b2_4cd9801bbc844d7f9fce1e92f80fa270~mv2.png"
+            alt="Logo Tienda"
+          />
+        </div>
+        <div className="footer">
+          <h3>Contacto</h3>
+          <ul>
+            <li>Teléfono: +569 123 456 89</li>
+            <li>Correo: tcg@contacto.cl</li>
+            <li>Dirección: Álvarez 1136</li>
+          </ul>
+        </div>
+      </div>
     </footer>
   );
 }
